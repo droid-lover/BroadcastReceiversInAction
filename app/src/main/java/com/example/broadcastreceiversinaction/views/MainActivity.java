88a729953d6c4584e3024165ea5a66dad7fc05c4
Sachin Rajput <sachin@Sachins-MacBook-Pro.local>
2018-03-20 15:56:21 +0530

    Added code to programmatically register receiver

--- app/src/main/java/com/example/broadcastreceiversinaction/views/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiversinaction/views/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.broadcastreceiversinaction.views;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.broadcastreceiversinaction.R;
+import com.example.broadcastreceiversinaction.receivers.MyGlobalReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
                 Intent customActionIntent = new Intent();
                 customActionIntent.setAction("CUSTOM_ACTION");
                 sendBroadcast(customActionIntent);
+
+                //We can register the Receiver Programmatically also like -
+//                IntentFilter customActionIntent = new IntentFilter("CUSTOM_ACTION");
+//
+//                MyGlobalReceiver myGlobalReceiver = new MyGlobalReceiver();
+//                registerReceiver(myGlobalReceiver,customActionIntent);
             }
         });
     }
